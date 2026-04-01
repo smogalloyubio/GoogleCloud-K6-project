f9bc694c3e723d48d985bdab103d681780868d7b
smogalloyubio <smogalloyubio@gmail.com>
2026-04-01 18:09:29 +0000

    update the service address

--- k6/smoke.js
+++ k6/smoke.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-const BASE_URL = __ENV.BASE_URL || 'http://app:3000';
+const BASE_URL = __ENV.BASE_URL || 'http://my-app:3000';
 
 export const options = {
   vus: 2,
